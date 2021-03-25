ab3a08d7bed2cbcf635939e725dd4aaf2d73bd52
annsudhart <asudhart@ucsd.edu>
2021-03-25 14:23:33 -0700

    we got a camera now

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native';
+// import { Camera } from 'expo';
 
 const HomeScreen = ({ navigation }) => {
   return (
