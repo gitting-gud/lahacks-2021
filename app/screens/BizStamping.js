66668372dd21d168d7f7b60e262a7c38c1cd2437
annsudhart <asudhart@ucsd.edu>
2021-03-27 15:58:00 -0700

    layout fixes for stamping approval

--- app/screens/BizStamping.js
+++ app/screens/BizStamping.js
@@ -1,36 +1,61 @@
 import React, {useState, useEffect} from 'react';
-import BizStamping from '../components/BizStamping';
+import CameraObject from '../components/Camera';
 import { View, Text, Button } from 'react-native';
+import { Input } from 'react-native-elements';
 
 const BizStampingScreen = ({ navigation }) => {
 
   const [data, setData] = useState('Scanning...');
+  const [stampReady, setStampReady] = useState(false);
+  
   const displayData = (data) => {
     // get text value, check to make sure it is in database, then enable stamp button
     setData(data.data);
+    setStampReady(true);
   }
+  const handleCodeInput = (textInput) => {
+    if (textInput.length == 6) {
+      // check if code is in database
+      setData(textInput);
+      setStampReady(true);
+    }
+  }
+
   return (
     <View style={{
         flex: 1,
         justifyContent: 'center',
+        marginTop:300,
         alignItems: 'center'
       }}>
-      <Text>This is the business stamping screen!</Text>
-      <BizStamping
+      <CameraObject
         readData={displayData}
       />
+      <Text>{data}</Text>
+      <Text>Or, enter the code here</Text>
+      <Input 
+        placeholder='123456'
+        containerStyle={{
+          paddingRight: 0,
+          paddingLeft: 0,
+          width: 75,
+        }}
+        inputContainerStyle={{
+          width: 75,
+        }}
+        inputStyle={{
+          textAlign: 'center',
+          width: 75,
+        }}
+        onChangeText={(newValue) => handleCodeInput(newValue)}
+      />
       <Button
-          title="Go to HomeScreen"
-          onPress={() =>
-            navigation.navigate('HomeScreen', /* { name: 'Jane' } */)
-          }></Button>
-          <Button
-          title="Stamp"
-          onPress={() =>
-            // deliver stamp 
-            navigation.navigate('UserScreen', /* { name: 'Jane' } */)
-          }></Button>
-          <Text>{data}</Text>
+        disabled={!stampReady}
+        title="Stamp"
+        onPress={() =>
+        // deliver stamp 
+        navigation.navigate('UserScreen', /* { name: 'Jane' } */)
+      }></Button>
     </View>
   );
 }
