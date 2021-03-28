bc12ead0fd1ca6efff4d71e04597f9669a726d57
annsudhart <asudhart@ucsd.edu>
2021-03-27 23:01:41 -0700

    refactored form input code

--- app/screens/NewStampScreen.js
+++ app/screens/NewStampScreen.js
@@ -1,6 +1,7 @@
 import React, {useState } from 'react';
 import { View, Text } from 'react-native';
 import { Button, Input } from 'react-native-elements';
+import FormInput from '../components/FormInput';
 
 const BizStampingScreen = ({ navigation }) => {
 
@@ -11,113 +12,26 @@ const BizStampingScreen = ({ navigation }) => {
         justifyContent: 'center',
         paddingLeft: 20,
       }}>
-      <Text>Emoji/single character stamp design</Text>
-      <Input 
-        placeholder='⭐'
-        containerStyle={{
-          paddingRight: 0,
-          paddingLeft: 0,
-          width: 50,
-        }}
-        inputContainerStyle={{
-            borderColor: '#71AAFF',
-            borderRadius: 12,
-            borderWidth: 1,
-            marginTop: 10,
-            width: 50,
-        }}
-        inputStyle={{
-          textAlign: 'center',
-          width: 50,
-        }}
-        onChangeText={(newValue) => handleCodeInput(newValue)}
-      />
-      <Text>Number of Stamps per Card</Text>
-      <Input 
-        placeholder='8'
-        containerStyle={{
-          borderColor: '#71AAFF',
-          paddingRight: 0,
-          paddingLeft: 0,
-          width: 50,
-        }}
-        inputContainerStyle={{
-          borderColor: '#71AAFF',
-          borderRadius: 12,
-          borderWidth: 1,
-          marginTop: 10,
-          width: 50,
-        }}
-        inputStyle={{
-          textAlign: 'center',
-          width: 50,
-        }}
-        onChangeText={(newValue) => handleCodeInput(newValue)}
-      />
-      <Text>Name of Stamp Card Deal</Text>
-      <Input 
-        placeholder='Spend $15+ 8 times and get 15% off next purchase'
-        containerStyle={{
-          paddingRight: 0,
-          paddingLeft: 0,
-          width: 300,
-        }}
-        inputContainerStyle={{
-            borderColor: '#71AAFF',
-            borderRadius: 12,
-            borderWidth: 1,
-            marginTop: 10,
-            width: 300,
-        }}
-        inputStyle={{
-          textAlign: 'center',
-          width: 300,
-        }}
-        onChangeText={(newValue) => handleCodeInput(newValue)}
-      />
-      <Text>Deal Details</Text>
-      <Input 
-        placeholder='Deal details, expiration date, any other important information goes here'
-        containerStyle={{
-          paddingRight: 0,
-          paddingLeft: 0,
-          width: 300,
-        }}
-        inputContainerStyle={{
-            borderColor: '#71AAFF',
-            borderRadius: 12,
-            borderWidth: 1,
-            marginTop: 10,
-            width: 300,
-        }}
-        inputStyle={{
-          textAlign: 'center',
-          width: 50,
-          height: 30
-        }}
-        onChangeText={(newValue) => handleCodeInput(newValue)}
-      />
-      <Text>Partner with other Organizations? (Invite will be sent)</Text>
-      <Input 
-        placeholder='Enter Organization code here'
-        containerStyle={{
-          paddingRight: 0,
-          paddingLeft: 0,
-          width: 250,
-        }}
-        inputContainerStyle={{
-            borderColor: '#71AAFF',
-            borderRadius: 12,
-            borderWidth: 1,
-            marginTop: 10,
-            width: 250,
-        }}
-        inputStyle={{
-          textAlign: 'center',
-          width: 250,
-        }}
-        onChangeText={(newValue) => handleCodeInput(newValue)}
-      />
+      <FormInput 
+        textTitle='Single character stamp'
+        placeholderText='⭐'
+        width={50} />
+      <FormInput 
+        textTitle='Number of Stamps per Card'
+        placeholderText='8'
+        width={50} />
+      <FormInput 
+        textTitle='Name of Stamp Card Deal'
+        placeholderText='Spend $15+ 8 times and get 15% off next purchase'
+        width={300} />
+      <FormInput 
+        textTitle='Deal Details'
+        placeholderText='Deal details, expiration date, any other important information goes here'
+        width={300} />
+      <FormInput 
+        textTitle='Partner with other Organizations? (Invite will be sent)'
+        placeholderText='Enter Organization code here'
+        width={250} />
       <Button
         title="Save and Confirm"
         onPress={() =>
